319c87928c9c03fc7697bf6a8288ce99ebfb41d6
Pranav Vashi <neobuddy89@gmail.com>
2024-11-01 02:38:55 +0700

    Updater: Change package name to avoid conflict
    Change-Id: I55d6c041e08323b074db636b861384e53c5cc508
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "id.afterlife.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
